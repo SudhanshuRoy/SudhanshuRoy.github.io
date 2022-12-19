32ef34db5cef8216a6b2a901542a201f38bd3596
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2022-12-19 18:55:49 +0530

    added

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
+//     $(window).scroll(function () {
       // sticky navbar on scroll script
-      if (this.scrollY > 20) {
-        $(".navbar").addClass("sticky");
-      } else {
-        $(".navbar").removeClass("sticky");
-      }
+      // if (this.scrollY > 20) {
+      //   $(".navbar").addClass("sticky");
+      // }
+      //  else {
+      //   $(".navbar").removeClass("sticky");
+      // }
   
       // scroll-up button show/hide script
-      if (this.scrollY > 500) {
-        $(".scroll-up-btn").addClass("show");
-      } else {
-        $(".scroll-up-btn").removeClass("show");
-      }
-    });
+    //   if (this.scrollY > 500) {
+    //     $(".scroll-up-btn").addClass("show");
+    //   } else {
+    //     $(".scroll-up-btn").removeClass("show");
+    //   }
+    // });
   
     // slide-up script
-    $(".scroll-up-btn").click(function () {
-      $("html").animate({ scrollTop: 0 });
-      // removing smooth scroll on slide-up button click
-      $("html").css("scrollBehavior", "auto");
-    });
+    // $(".scroll-up-btn").click(function () {
+    //   $("html").animate({ scrollTop: 0 });
+    //   // removing smooth scroll on slide-up button click
+    //   $("html").css("scrollBehavior", "auto");
+    // });
   
-    $(".navbar .menu li a").click(function () {
-      // applying again smooth scroll on menu items click
-      $("html").css("scrollBehavior", "smooth");
-    });
+    // $(".navbar .menu li a").click(function () {
+    //   // applying again smooth scroll on menu items click
+    //   $("html").css("scrollBehavior", "smooth");
+    // });
   
     // toggle menu/navbar script
     $(".menu-btn").click(function () {
@@ -33,21 +34,7 @@ $(document).ready(function () {
       $(".menu-btn i").toggleClass("active");
     });
   
-    // typing text animation script
-    var typed = new Typed(".typing", {
-      strings: ["Back-End Developer", "Believer"],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true,
-    });
-  
-    var typed = new Typed(".typing-2", {
-      strings: ["Back-End Developer", "Believer"],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true,
-    });
-  
+   
     // owl carousel script
     $(".carousel").owlCarousel({
       margin: 20,
